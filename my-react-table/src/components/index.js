321764d498e5033ab4ba9bb8607375c009dd2eb1
hilmi sarioglu <hilmisarioglu2213@gmail.com>
2024-04-16 15:50:43 +0200

    modal

--- my-react-table/src/components/index.js
+++ my-react-table/src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Table } from "./table/Table/Table";
-export { default as Modal } from "./modal/Modal";
+export { default as Modal } from "./modal2/Modal";
 
 export { default as Form } from "./form2/Form";
